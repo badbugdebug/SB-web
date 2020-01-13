4bf16c99e8eeaaadbf5da4f43a95e7d639c2a678
jongminPark <whdals913@gmail.com>
2020-01-13 23:23:32 +0900

    add controller

---
+++ src/main/java/com/emile/dev/web/HelloController.java
@@ -0,0 +1,13 @@
+package com.emile.dev.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+  @GetMapping("/hello")
+  public String hello() {
+    return "hello";
+  }
+}
