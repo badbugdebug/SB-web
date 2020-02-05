270930b2b752c45c0d29f50cff2bd6222840879c
jongminPark <whdals913@gmail.com>
2020-02-05 23:58:23 +0900

    logger 세팅

--- src/main/java/com/emile/dev/web/HelloController.java
+++ src/main/java/com/emile/dev/web/HelloController.java
@@ -1,10 +1,12 @@
 package com.emile.dev.web;
 
 import com.emile.dev.web.dto.HelloReponseDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class HelloController {
 
