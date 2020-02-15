4c4acec5db2cc5c7b5f3eb9e4b732ae5eaec03fb
jongminPark <whdals913@gmail.com>
2020-02-15 23:20:57 +0900

    log 제거, devtool 주석

--- src/main/java/com/emile/dev/web/IndexController.java
+++ src/main/java/com/emile/dev/web/IndexController.java
@@ -5,7 +5,6 @@ import com.emile.dev.config.auth.dto.SessionUser;
 import com.emile.dev.service.PostsService;
 import com.emile.dev.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,7 +13,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import javax.servlet.http.HttpSession;
 
 
-@Slf4j
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
@@ -27,7 +25,6 @@ public class IndexController {
         model.addAttribute("posts", postsService.findAllDesc());
 
         if (user != null) {
-            log.info(user.toString());
             // win 10 -> userName 환경변수 충돌
             model.addAttribute("name", user.getName());
         }
