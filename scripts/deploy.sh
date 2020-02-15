73c624ae6f6c1fb21c2e58584630a1f98fde10e3
jongminPark <whdals913@gmail.com>
2020-02-15 21:51:10 +0900

    oauth complete

---
+++ scripts/deploy.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+REPOSITORY=/home/ec2-user/app/step1
+PROJECT_NAME=SB-web-springboot
+
+cd $REPOSITORY/$PROJECT_NAME/
+
+echo "> git pull"
+
+git pull
+
+echo "> project build start"
+
+./gradlew build
+
+echo "> step1 디렉토리 이동"
+
+cd $REPOSITORY
+
+echo "> Build 파일 복사"
+
+cp $REPOSITORY/$PROJECT_NAME/build/libs/*.jar $REPOSITORY/
+
+echo "> 현재 구동중인 애플리케이션 pid 확인"
+
+#프로세스 아이디 만 추출
+CURRENT_PID=$(pgrep -f ${PROJECT_NAME}*.jar)
+
+echo "현재 구동중인 어플리케이션 pid: $CURRENT_PID"
+
+if [ -z "$CURRENT_PID" ]; then
+    echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
+else
+    echo "> kill -15 $CURRENT_PID"
+    kill -15 $CURRENT_PID
+    sleep 5
+fi
+
+echo "> 새 어플리케이션 배포"
+
+#가장 나중의 파일 저장
+JAR_NAME=$(ls -tr $REPOSITORY/ |grep *.jar | tail -n 1)
+
+echo "> JAR Name: $JAR_NAME"
+
+nohup java -jar $REPOSITORY/$JAR_NAME 2>&1 &
